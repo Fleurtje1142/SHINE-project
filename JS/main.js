66d30f6d01728ab0e7dc2cbdf3e41399e16d15ef
Fleurtje1142 <f.p.a.vangils@icloud.com>
2024-03-20 16:33:06 +0100

    tried to make it better still not walking with buttons

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,4 @@
-document.getElementById("mainTitle").innerText =
-  "Point and Click adventure game";
+document.getElementById("mainTitle").innerText = "ADVENTURE GAME";
 // Game State
 let gameState = {
   inventory: [],
@@ -8,7 +7,8 @@ let gameState = {
 };
 
 //localStorage.removeItem("gameState");
-if (Storage) {
+/**
+ * if (Storage) {
   if (localStorage.gameState) {
     // uses LocalStorage gameState string and convert it to an object. Then store it into gameState.
     gameState = JSON.parse(localStorage.gameState);
@@ -17,6 +17,7 @@ if (Storage) {
     localStorage.setItem("gameState", JSON.stringify(gameState));
   }
 }
+ */
 
 //Game window reference
 const gameWindow = document.getElementById("gameWindow");
@@ -26,16 +27,20 @@ const sec = 1000;
 //Main Character
 const mainCharacter = document.getElementById("hero");
 const offsetCharacter = 16;
+const doggie = document.getElementById("doggie");
 
 //speech bubbles
 const heroSpeech = document.getElementById("heroSpeech");
 const counsterSpeech = document.getElementById("counterSpeech");
+const doggieSpeech = document.getElementById("doggieSpeech");
 //audio for dialog
 const heroAudio = document.getElementById("heroAudio");
 const counterAudio = document.getElementById("counterAudio");
+const doggieAudio = document.getElementById("doggieAudio");
 
 //avatar
 const counterAvatar = document.getElementById("counterAvatar");
+const doggieAvatar = document.getElementById("doggieAvatar");
 
 //Objects
 const tree1 = document.getElementById("squareTree");
@@ -46,6 +51,14 @@ if (gameState.keyPickedUp) {
 
 updateInventory(gameState.inventory, inventoryList);
 
+//movement
+let hero = document.querySelector("#hero");
+let x = 0;
+let y = 0;
+let speed = 10;
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
 gameWindow.onclick = function (e) {
   var rect = gameWindow.getBoundingClientRect();
   var x = e.clientX - rect.left;
@@ -54,7 +67,11 @@ gameWindow.onclick = function (e) {
   //TODO: calc offset based on character size
   //TODO: making dialog functionality
 
-  if (counterSpeech.style.opacity == 0 && heroSpeech.style.opacity == 0) {
+  if (
+    counterSpeech.style.opacity == 0 &&
+    doggieSpeech.style.opacity == 0 &&
+    heroSpeech.style.opacity == 0
+  ) {
     if (e.target.id !== "heroImage") {
       mainCharacter.style.left = x - offsetCharacter + "px";
       mainCharacter.style.top = y - offsetCharacter + "px";
@@ -128,6 +145,46 @@ gameWindow.onclick = function (e) {
         );
         setTimeout(function () {
           counterAvatar.style.opacity = 0;
+        }, 20 * sec);
+        //console.log("hey you.. wanna know where the key is? It's by the graves.");
+        break;
+
+      case "doggie":
+        showMessage(heroSpeech, "Hey a cute little doggie.", heroAudio);
+        setTimeout(function () {
+          doggieAvatar.style.opacity = 1;
+        }, 4 * sec);
+        setTimeout(
+          showMessage,
+          4.1 * sec,
+          doggieSpeech,
+          "WOEF WOEF WOEF ",
+          doggieAudio
+        );
+        setTimeout(
+          showMessage,
+          8.1 * sec,
+          heroSpeech,
+          "Owh dont get mad at me I'm just saying hi",
+          heroAudio
+        );
+        setTimeout(
+          showMessage,
+          12 * sec,
+          doggieSpeech,
+          "WOEF WOEF WOEF",
+          doggieAudio
+        );
+        setTimeout(
+          showMessage,
+          16 * sec,
+          heroSpeech,
+          "Oke bye bye now",
+          heroAudio
+        );
+        setTimeout(function () {
+          doggieAvatar.style.opacity = 0;
+          doggie.style.opacity = 0;
         }, 20 * sec);
         //console.log("hey you.. wanna know where the key is? It's by the graves.");
         break;
